87e4ba9a5350d8f4c76ef12bb989e88929c291ff
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-05-22 17:25:37 -0400

    Update xpl.js

--- xpl.js
+++ xpl.js
@@ -1,3 +1,28 @@
+var xplcodes = {
+popup: function(code) {alert(params[0]);},
+ask: function(code) {return prompt(params[0]);},
+
+};
+function readread(code,params) {
+return xplcodes[code.tagName](params);
+}
+function readline(code) {
+if (code.children.length > 0) {
+  var params = new Array();
+for(var z = 0; z<code.children.length;z++) {
+  params[z] = readline(code.children[z]);
+  
+}
+  return readread(code,params);
+} else {
+  var params = new Array();
+return readread(code,params);
+}
+}
+function xplscript(code) {
+readline(code.children[0]);
+}
+
 var parse = new DOMParser();
 var xml = function(code) {return parse.parseFromString(code,"text/xml");};
 function read(code) {
@@ -6,5 +31,11 @@ var xpl = xml(code);
   var scripts = xpl.getElementsByTagName("javascript");
   for (var i = 0; i<scripts.length; i++) {
   eval(scripts[i].innerHTML);
+    
+  }
+    var scripts = xpl.getElementsByTagName("xplscript");
+  for (var i = 0; i<scripts.length; i++) {
+  xplscript(scripts[i]);
+    
   }
 }
