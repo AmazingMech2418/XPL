5d70125499455a3fcc1054bbee1ecc7ffbcd7352
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-05-22 17:03:23 -0400

    Update xpl.js

--- xpl.js
+++ xpl.js
@@ -1,1 +1,6 @@
 var parse = new DOMParser();
+var xml = function(code) {return parse.parseFromString(code,"text/xml");};
+function read(code) {
+var xpl = xml(code);
+  document.documentElement.innerHTML = xpl.document.getElementsByTagName("html")[0];
+}
